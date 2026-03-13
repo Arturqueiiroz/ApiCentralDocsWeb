c546bcba3044b8839303320a7b03304fcb8e383a
ARTUR COSMO DE QUEIROZ <artur.cqueiroz@senacsp.edu.br>
2026-03-13 17:29:04 -0300

    CriarDocumento

---
+++ Model/DTO/CriarDocumentoDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiCentralDocsWeb.Model.DTO
+{
+    public class CriarDocumentoDTO
+    {
+        [Required]
+        public string Numero { get; set; } = string.Empty;
+        [Required]
+        public string OrgaoEmissor { get; set; } = string.Empty;
+        public DateTime DataEmissao { get; set; }
+        public string CidadeEmissao { get; set; } = string.Empty;
+        [Required]
+        public int UsuarioId { get; set; }
+        [Required]
+        public int TipoDocumentoId { get; set; }
+    }
+}
