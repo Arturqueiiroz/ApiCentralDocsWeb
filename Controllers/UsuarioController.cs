7d7a7428abd0210eba008816dc6a228aa9b8fe27
ARTUR COSMO DE QUEIROZ <artur.cqueiroz@senacsp.edu.br>
2026-03-23 14:51:25 -0300

    Alterando o Usuario controllers, e adicionando o Usuario Service

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using ApiCentralDocsWeb.Data;
 using ApiCentralDocsWeb.Model;
 using ApiCentralDocsWeb.Model.DTO;
+using ApiCentralDocsWeb.Services;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ApiCentralDocsWeb.Controllers
 {
@@ -10,26 +12,26 @@ namespace ApiCentralDocsWeb.Controllers
     [Route("api/[controller]")]
     public class UsuarioController : ControllerBase
     {
-        private readonly AppDbContext _context;
+        private readonly UsuarioService _usuarioService;
 
-        public UsuarioController(AppDbContext context)
+        public UsuarioController(UsuarioService usuarioService)
         {
-            _context = context;
+            _usuarioService = usuarioService;
         }
 
         [HttpGet("GetAll")]
         public async Task<IActionResult> GetAllUsuarios()
         {
-            var usuarios = await _context.Usuarios.Include(usuario => usuario.Documentos).ToListAsync();
+            List<Usuario> usuarios = await _usuarioService.GetAllUsuarios();
             return Ok(usuarios);
         }
 
         [HttpGet("GetById{id}")]
         public async Task<IActionResult> GetUsuarioById([FromRoute] int id)
         {
-            var usuario = await _context.Usuarios.FindAsync(id);
+            dynamic usuario = await _usuarioService.GetUsuarioById(id);
 
-            if (usuario == null)
+            if (usuario.Usuario == null)
 
             {
                 return BadRequest(new
@@ -46,130 +48,43 @@ namespace ApiCentralDocsWeb.Controllers
         public async Task<IActionResult> CriarUsuario([FromBody] CriarUsuarioDTO dadosUsuario)
         {
             if (!ModelState.IsValid)
-            {
                 return BadRequest(ModelState);
-            }
-
-            var usuarioExistente = await _context.Usuarios
-                .FirstOrDefaultAsync(usuario => usuario.CPF == dadosUsuario.CPF);
-
-            if (usuarioExistente != null)
-            {
-                return BadRequest($"Já existe um usuário com CPF {dadosUsuario.CPF}");
-            }
-
-            Usuario usuario = new Usuario
-            {
-                Nome = dadosUsuario.Nome,
-                CPF = dadosUsuario.CPF,
-                Email = dadosUsuario.Email,
-                Senha = dadosUsuario.Senha
-            };
-
-            _context.Usuarios.Add(usuario);
-
-            int resultado = await _context.SaveChangesAsync();
-
-            if (resultado > 0)
-                return Created($"Usuario {usuario.Nome} criado com sucesso!", usuario);
-
-            return BadRequest("Erro ao criar usuário");
+            var resultado = await _usuarioService.CriarUsuario(dadosUsuario);
+            if (resultado.Erro)
+                return BadRequest(resultado);
+            return Ok(resultado);
         }
-
         [HttpPut("AtualizarUsuario/{id}")]
-        public async Task<IActionResult> AtualizarUsuario([FromRoute] int id, [FromBody] AtualizarUsuarioDTO dadosUsuario)
+        public async Task<IActionResult> AtualizarUsuario(int id, AtualizarUsuarioDTO dadosUsuario)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            var usuario = await _context.Usuarios.FindAsync(id);
-            if (usuario == null)
-            {
-                return BadRequest(new
-                {
-                    Erro = true,
-                    Mensagem = $"Usuário com id {id} não encontrado"
-                });
-            }
-            usuario.Nome = dadosUsuario.Nome;
-            usuario.Email = dadosUsuario.Email;
-            usuario.Senha = dadosUsuario.Senha;
-            _context.Usuarios.Update(usuario);
-            int resultado = await _context.SaveChangesAsync();
-            if (resultado > 0)
-                return Ok(new
-                {
-                    Mensagem = $"Usuário com id {id} foi atualizado com sucesso",
-                    UsuarioAtualizado = usuario
-                });
-            return BadRequest("Erro ao atualizar usuário");
+            var resultado = await _usuarioService.AtualizarUsuario(id, dadosUsuario);
+
+            if (resultado.Erro)
+
+                return BadRequest(resultado);
+
+            return Ok(resultado);
         }
 
         [HttpDelete("DeletarUsuario/{id}")]
-        public async Task<IActionResult> DeletarUsuario([FromRoute] int id)
+        public async Task<IActionResult> DeletarUsuario(int id)
         {
-            var usuario = await _context.Usuarios.FindAsync(id);
+            var resultado = await _usuarioService.DeletarUsuario(id);
 
-            if (usuario == null)
-            {
-                return BadRequest(new
-                {
-                    Erro = true,
-                    Mensagem = $"Usuário com id {id} não encontrado"
-                });
-            }
+            if (resultado.Erro)
+                return BadRequest(resultado);
 
-            _context.Usuarios.Remove(usuario);
-
-            int resultado = await _context.SaveChangesAsync();
-
-            if (resultado > 0)
-                return Ok(new
-                {
-                    Mensagem = $"Usuário com id {id} foi deletado com sucesso"
-                });
-
-            return BadRequest("Erro ao deletar usuário");
+            return Ok("Usuário deletado com sucesso");
         }
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginDTO dados)
         {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+            var resultado = await _usuarioService.Login(dados);
 
-            var usuario = await _context.Usuarios
-                .Include(ussuario => ussuario.Documentos)
-                    .ThenInclude(ddocumento => ddocumento.TipoDocumento)
-                .FirstOrDefaultAsync(ussuario => ussuario.Email == dados.Email && ussuario.Senha == dados.Senha);
+            if (resultado.Erro)
+                return Unauthorized(resultado);
 
-            if (usuario == null)
-            {
-                return Unauthorized(new
-                {
-                    Erro = true,
-                    Mensagem = "Email ou senha inválidos"
-                });
-            }
-
-            return Ok(new
-            {
-                Mensagem = "Login realizado com sucesso",
-                Usuario = new
-                {
-                    usuario.Id,
-                    usuario.Nome,
-                    usuario.Email,
-                    Documentos = usuario.Documentos.Select(documentos => new
-                    {
-                        documentos.Id,
-                        documentos.Numero,
-                        documentos.OrgaoEmissor,
-                        documentos.CidadeEmissao,
-                        Tipo = documentos.TipoDocumento.Nome
-                    })
-                }
-            });
+            return Ok(resultado);
         }
     }
 }
